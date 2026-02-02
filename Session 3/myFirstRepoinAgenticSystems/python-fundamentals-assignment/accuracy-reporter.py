a3866dda8806b4773f491efbedb8ca1344449df0
Bhavik Sanghvi <43782548+bhavik1611@users.noreply.github.com>
2026-02-02 17:24:55 +0530

    Delete Session 3/myFirstRepoinAgenticSystems directory

--- Session 3/myFirstRepoinAgenticSystems/python-fundamentals-assignment/accuracy-reporter.py
+++
@@ -1,8 +0,0 @@
-accuracy = input("Enter the accuracy of the model: ")
-# validate if the input is numeric
-try:
-    accuracy_val = float(accuracy)
-    print(f"Model accuracy is {accuracy_val:.2f}")
-except ValueError:
-    print("Invalid input. Please enter a numeric value.")
-    exit()
--- Session 3/myFirstRepoinAgenticSystems/python-fundamentals-assignment/age-calculator.py
+++
@@ -1,4 +0,0 @@
-current_year = 2026
-birth_year = input("Enter your birth year: ")
-age = current_year - int(birth_year)
-print(f"You are {age} years old")
